5e79065e56205e6ff308587410caafe73513c0bb
mayurphumatiya <mayurphumatiya13@gmail.com>
2023-09-08 17:30:43 +0530

    conflicts resolved

--- app/(dashboard)/(routes)/dashboard/page.tsx
+++ app/(dashboard)/(routes)/dashboard/page.tsx
@@ -1,6 +1,7 @@
-import { UserButton } from "@clerk/nextjs";
-
 const DashboardPage = () => {
-  return <p>Dashboard Page (Protected) </p>;
-};
-export default DashboardPage;
+  return (
+    <p>Dashboard Page (Protected) </p>
+    )
+  }
+  export default DashboardPage
+
