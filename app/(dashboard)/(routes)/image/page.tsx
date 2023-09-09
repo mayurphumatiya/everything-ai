d3589de87a90c5fd06b5f531f3a20b4ed315b1ac
mayurphumatiya <mayurphumatiya13@gmail.com>
2023-09-09 18:18:52 +0530

    Image generation functionality added

--- app/(dashboard)/(routes)/image/page.tsx
+++ app/(dashboard)/(routes)/image/page.tsx
@@ -3,7 +3,7 @@
 import axios from "axios";
 import * as z from "zod";
 import Heading from "@/components/heading";
-import { ImageIcon } from "lucide-react";
+import { Download, ImageIcon } from "lucide-react";
 import { useForm } from "react-hook-form";
 import { zodResolver } from "@hookform/resolvers/zod";
 
@@ -23,6 +23,8 @@ import {
   SelectTrigger,
   SelectValue,
 } from "@/components/ui/select";
+import { Card, CardFooter } from "@/components/ui/card";
+import Image from "next/image";
 
 const ImagePage = () => {
   const router = useRouter();
@@ -115,7 +117,7 @@ const ImagePage = () => {
                   </FormItem>
                 )}
               />
-               <FormField
+              <FormField
                 name="resolution"
                 control={form.control}
                 render={({ field }) => (
@@ -160,7 +162,25 @@ const ImagePage = () => {
           {images.length === 0 && !isLoading && (
             <Empty label="No Images generated." />
           )}
-          <div>Images will be rendered here.</div>
+          <div className="grid grid-cols-1 md:grid-cols-2 lg:grid-cols-3 xl:grid-cols-4 gap-4 mt-8 ">
+            {images.map((src) => (
+              <Card key={src} className="rounded-lg overflow-hidden">
+                <div className="relative aspect-square">
+                  <Image fill src={src} alt="image" />
+                </div>
+                <CardFooter className="p-2">
+                  <Button
+                    onClick={() => window.open(src)}
+                    variant="secondary"
+                    className="w-full"
+                  >
+                    <Download className="h-4 w-4 mr-2" />
+                    Download
+                  </Button>
+                </CardFooter>
+              </Card>
+            ))}
+          </div>
         </div>
       </div>
     </div>
