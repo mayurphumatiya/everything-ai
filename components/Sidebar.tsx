bdb9f276ca3dbc49cc09c96a41b4cd20db38cb2c
mayurphumatiya <mayurphumatiya13@gmail.com>
2023-09-10 14:22:30 +0530

    counter added for free trial

--- components/Sidebar.tsx
+++ components/Sidebar.tsx
@@ -7,6 +7,7 @@ import { usePathname } from "next/navigation";
 
 import { cn } from "@/lib/utils";
 import { Code, ImageIcon, LayoutDashboard, MessageSquare, Music, Settings, VideoIcon } from "lucide-react";
+import FreeCounter from "./free-counter";
 
 const montserrat = Montserrat({ weight: "600", subsets: ["latin"] });
 
@@ -54,7 +55,11 @@ const routes = [
   }
 ]
 
-const Sidebar = () => {
+interface SidebarProps{
+  apiLimitCount:number;
+}
+
+const Sidebar = ({apiLimitCount = 0} : SidebarProps) => {
 
   const pathname = usePathname();
 
@@ -78,6 +83,7 @@ const Sidebar = () => {
           ))}
         </div>
       </div>
+      <FreeCounter apiLimitCount={apiLimitCount} />
     </div>
   );
 };
