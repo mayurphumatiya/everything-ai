6c20d2e526827c6bc2211501566949b0c774fbd2
mayurphumatiya <mayurphumatiya13@gmail.com>
2023-09-10 20:47:17 +0530

    crisp integration

--- app/layout.tsx
+++ app/layout.tsx
@@ -4,6 +4,7 @@ import { Inter } from "next/font/google";
 import { ClerkProvider } from "@clerk/nextjs";
 import { ModalProvider } from "@/components/modal-provider";
 import { ToasterProvider } from "@/components/toaster-provider";
+import { CrispProvider } from "@/components/crisp-provider";
 
 const inter = Inter({ subsets: ["latin"] });
 
@@ -24,6 +25,7 @@ export default function RootLayout({
           <ModalProvider />
           <ToasterProvider />
           {children}
+          <CrispProvider />
         </body>
       </html>
     </ClerkProvider>
